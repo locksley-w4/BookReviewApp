1a9fee51bbc209ae36f987981b48839d892dc0c7
aaa <94060212ad@gmail.com>
2025-10-30 05:27:37 -0400

    Tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,7 +9,7 @@ const public_users = express.Router();
 public_users.post("/register", (req, res) => {
     try {
         const { username, password } = req.body;
-        if(!username || !password) {
+        if (!username || !password) {
             return res.status(401).json({ message: "Username and password must not be empty" });
         }
         const isAvailable = !users[username];
@@ -29,26 +29,35 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    return res.status(200).json(books)
+public_users.get('/', async function (req, res) {
+    const bookList = await new Promise((resolve) => {
+        resolve(books) // substitute with request to DB, when implemented
+    });
+    res.status(200).json(bookList);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
-    const result = books[isbn];
+    const bookList = await new Promise((resolve) => {
+        resolve(books) // substitute with request to DB, when implemented
+    });
+    const result = bookList[isbn];
     if (result)
         return res.status(200).json(result);
     return res.status(404).json({ message: `Book with ISBN ${isbn} was not found.` })
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+    const bookList = await new Promise((resolve) => {
+        resolve(books) // substitute with request to DB, when implemented
+    });
     const filtered = [];
-    for (const i in books) {
-        if (Object.hasOwnProperty.call(books, i)) {
-            const book = books[i];
+    for (const i in bookList) {
+        if (Object.hasOwnProperty.call(bookList, i)) {
+            const book = bookList[i];
             if (book?.author === author) filtered.push(book);
         }
     }
@@ -56,12 +65,15 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title.toLocaleLowerCase()
+    const bookList = await new Promise((resolve) => {
+        resolve(books) // substitute with request to DB, when implemented
+    });
     const filtered = [];
-    for (const i in books) {
-        if (Object.hasOwnProperty.call(books, i)) {
-            const book = books[i];
+    for (const i in bookList) {
+        if (Object.hasOwnProperty.call(bookList, i)) {
+            const book = bookList[i];
             if (book.title.toLowerCase().includes(title)) filtered.push(book);
         }
     }
